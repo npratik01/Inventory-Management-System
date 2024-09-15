d225ce80df910031309ec2c902f4d679864f8a6a
Pratik Vijay Nikam <2241038@gcoej.ac.in>
2024-09-15 21:31:25 +0530

    billing is going on

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,14 @@
-        self.show()
+def show(self):
+        con=sqlite3.connect(database='ims.db')
+        cur=con.cursor()
+        try:
+            cur.execute("select * from product")
+            rows=cur.fetchall()
+            self.product_tabel.delete(*self.product_tabel.get_children())
+            for row in rows:
+                self.product_tabel.insert('',END,values=row)
 
-#======= Search Button ===============================
-    def search(self):
+
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to : {str(ex)}",parent=self.root)
+            
