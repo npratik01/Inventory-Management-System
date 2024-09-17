dd57fbd4f81e61a8c99c42fc07da2a7582a73007
Shubham07badgujar <shubhambadgujari076@gmail.com>
2024-09-17 15:05:29 +0530

    lkrnlglsdg

--- create_db.py
+++ create_db.py
@@ -1,24 +1,17 @@
 import sqlite3
-
 def create_db():
     con = sqlite3.connect(database=r'ims.db')
-    cur = con.cursor()
-    
-    # Create member table
+    cur=con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS member(memid INTEGER PRIMARY KEY AUTOINCREMENT, name text, email text, gender text, contact text, dob text, pass text, utype text)")
+    con.commit()
+
+    cur.execute("CREATE TABLE IF NOT EXISTS supplier(invoice INTEGER PRIMARY KEY AUTOINCREMENT, name text, contact text, desc text)")
+    con.commit()
+                                                        # "Product Name","Invoice number","Invoice date","Price","Total Invoice Amount","Supplier Name","Vendor MO NO","Email of vendor","R Student Name","R Std Mo No","Current position","Current Mo No"
+    # cur.execute("CREATE TABLE IF NOT EXISTS product(Invoice_number INTEGER PRIMARY KEY AUTOINCREMENT,Product Name text,Invoice date text,Price text,Total Invoice Amount text,Supplier Name text,Vendor MO NO text,Email of vendor text,R Student Name text,R Std Mo No text,Current position text,Current Mo No text)")
+    # con.commit()
+
     cur.execute("""
-<<<<<<< HEAD
-        CREATE TABLE IF NOT EXISTS member (
-            memid INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            email TEXT,
-            gender TEXT,
-            contact TEXT,
-            dob TEXT,
-            pass TEXT,
-            utype TEXT
-        )
-    """)
-=======
             CREATE TABLE IF NOT EXISTS product(
                 Invoice_number INTEGER PRIMARY KEY AUTOINCREMENT, 
                 [Product Name] TEXT, 
@@ -36,40 +29,8 @@ def create_db():
                 
             )
         """)
->>>>>>> 35ba6a2fcd2a8a523a1b5ee14c47a87ee9061ae9
     con.commit()
     
-    # Create supplier table
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS supplier (
-            invoice INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            contact TEXT,
-            desc TEXT
-        )
-    """)
-    con.commit()
     
-    # Create product table
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS product (
-            Invoice_number INTEGER PRIMARY KEY AUTOINCREMENT,
-            [Product Name] TEXT,
-            [Invoice date] TEXT,
-            Price INTEGER,
-            qty REAL,
-            [Total Invoice Amount] TEXT,
-            [Supplier Name] TEXT,
-            [Vendor MO NO] TEXT,
-            [Email of vendor] TEXT,
-            [R Student Name] TEXT,
-            [R Std Mo No] TEXT,
-            [Current position] TEXT,
-            [Current Mo No] TEXT
-        )
-    """)
-    con.commit()
-    
-    con.close()
 
 create_db()
